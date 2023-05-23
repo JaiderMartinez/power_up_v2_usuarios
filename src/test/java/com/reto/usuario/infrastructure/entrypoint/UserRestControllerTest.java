0df8f913a8737a79c0f94d7ce3c88344677e3ee6
JaiderMartinez <smithsaemirto45@gmail.com>
2023-05-22 19:55:48 -0500

    Agregando test de integración

--- src/test/java/com/reto/usuario/infrastructure/entrypoint/UserRestControllerTest.java
+++ src/test/java/com/reto/usuario/infrastructure/entrypoint/UserRestControllerTest.java
@@ -3,7 +3,9 @@ package com.reto.usuario.infrastructure.entrypoint;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.reto.usuario.application.dto.request.UserRequestDto;
 import com.reto.usuario.infrastructure.drivenadapter.entity.RolEntity;
+import com.reto.usuario.infrastructure.drivenadapter.entity.UserEntity;
 import com.reto.usuario.infrastructure.drivenadapter.repository.IRolRepositoryMysql;
+import com.reto.usuario.infrastructure.drivenadapter.repository.IUserRepositoryMysql;
 import com.reto.usuario.infrastructure.exceptionhandler.ExceptionResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -35,6 +37,9 @@ class UserRestControllerTest {
     @Autowired
     private IRolRepositoryMysql rolRepositoryMysql;
 
+    @Autowired
+    private IUserRepositoryMysql userRepositoryMysql;
+
     private static final String USERNAME_ADMIN = "admin@dmin.com";
     private static final String PASSWORD = "123";
     private static final String ROLE_ADMIN = "ADMINISTRADOR";
@@ -65,7 +70,7 @@ class UserRestControllerTest {
 
     @WithMockUser(username = USERNAME_ADMIN, password = PASSWORD, roles = {ROLE_ADMIN})
     @Test
-    void test_registerUserAsOwner_withUserRequestDtoAndInvalidEmailProperty_shouldThrowStatusBaqRequest() throws Exception {
+    void test_registerUserAsOwner_withFieldEmailIsInvalidInUserRequestDto_shouldThrowStatusBadRequest() throws Exception {
         UserRequestDto userOwner = new UserRequestDto();
         userOwner.setName("Jose");
         userOwner.setLastName("Martinez");
@@ -79,5 +84,63 @@ class UserRestControllerTest {
                         .content(objectMapper.writeValueAsString(userOwner)))
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.message").value(ExceptionResponse.INVALID_EMAIL_FORMAT.getMessage()));
+    }
+
+    @WithMockUser(username = USERNAME_ADMIN, password = PASSWORD, roles = {ROLE_ADMIN})
+    @Test
+    void test_registerUserAsOwner_withFieldsEmptyInUserRequestDto_shouldThrowStatusBadRequest() throws Exception {
+        UserRequestDto userOwner = new UserRequestDto();
+        userOwner.setName("");
+        userOwner.setLastName("");
+        userOwner.setIdentificationDocument(null);
+        userOwner.setCellPhone("");
+        userOwner.setEmail("email@example.com");
+        userOwner.setPassword("");
+
+        mockMvc.perform(post(USER_OWNER_API_PATH)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userOwner)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value(ExceptionResponse.EMPTY_FIELDS.getMessage()));
+    }
+
+    @WithMockUser(username = USERNAME_ADMIN, password = PASSWORD, roles = {ROLE_ADMIN})
+    @Test
+    void test_registerUserAsOwner_withInvalidCellPhoneInUserRequestDto_shouldThrowStatusBadRequest() throws Exception {
+        UserRequestDto userOwner = new UserRequestDto();
+        userOwner.setName("Jose");
+        userOwner.setLastName("Martinez");
+        userOwner.setIdentificationDocument(12323435345L);
+        userOwner.setIdentificationDocument(null);
+        userOwner.setCellPhone("+5792385492378345");
+        userOwner.setEmail("owner@owner.com");
+        userOwner.setPassword(PASSWORD);
+
+        mockMvc.perform(post(USER_OWNER_API_PATH)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userOwner)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value(ExceptionResponse.EMPTY_FIELDS.getMessage()));
+    }
+
+    @WithMockUser(username = USERNAME_ADMIN, password = PASSWORD, roles = {ROLE_ADMIN})
+    @Test
+    void test_registerUserAsOwner_withExistingEmailInUserRequestDto_shouldThrowStatusConflict() throws Exception {
+        userRepositoryMysql.save(new UserEntity(1L, "Jose", "Martinez", 12323435345L,
+                "3154579374", "owner@owner.com", PASSWORD, new RolEntity(1L, "PROPIETARIO", "Restaurant owner")));
+
+        UserRequestDto userOwner = new UserRequestDto();
+        userOwner.setName("Jose");
+        userOwner.setLastName("Martinez");
+        userOwner.setIdentificationDocument(12323435345L);
+        userOwner.setCellPhone("3154579374");
+        userOwner.setEmail("owner@owner.com");
+        userOwner.setPassword(PASSWORD);
+
+        mockMvc.perform(post(USER_OWNER_API_PATH)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userOwner)))
+                .andExpect(status().isConflict())
+                .andExpect(jsonPath("$.message").value(ExceptionResponse.EMAIL_EXISTS.getMessage()));
     }
 }
